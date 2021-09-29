de40eba45a96352d9a0e86de766254afa3612760
dmelga1 <91485662+dmelga1@users.noreply.github.com>
2021-09-29 17:39:29 +0200

    AEA2. nodeYARGS

---
+++ AEA2. nodeYARGS/helper/hores.js
@@ -0,0 +1,20 @@
+require('colors');
+const { rejects } = require('assert');
+const fs =require('fs'); 
+
+const crearHores = (nom = 'no_name',hores = 0) => {
+
+    let sortida = "";
+
+        sortida = `Alumne: ${nom}::Hores: ${hores}`;
+        fs.writeFileSync(`${nom}.txt`, sortida);
+
+        console.log("============================".red);
+        console.log(`El Alumne ${nom} , ha fet ${hores}`);
+        console.log("============================".red);
+   
+}
+
+module.exports = {
+    crearHores,
+}
