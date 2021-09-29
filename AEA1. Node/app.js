47d4d43a0df2e2b85b062c08c2868ef56e97cce5
dmelga1 <91485662+dmelga1@users.noreply.github.com>
2021-09-29 17:45:31 +0200

    AEA1. Node

---
+++ AEA1. Node/app.js
@@ -0,0 +1,12 @@
+require("colors");
+
+const{ crearAlumne } = require('./helpers/hores');
+console.log(process.argv);
+
+let nom = process.argv[2];
+nom = nom.split('='); 
+
+let hores = process.argv[3];
+hores = hores.split("=")[1];
+
+crearAlumne(nom, hores);
